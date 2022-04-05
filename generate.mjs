5bf1fd61a83f79a0d5e1ec78760d2cc30edec6a8
matthias <matthias.fuchs>
2022-04-05 08:21:50 +0200

    parameter

--- generate.mjs
+++ generate.mjs
@@ -1,10 +1,14 @@
 import get_file_data from './scripts/getfilestructure.mjs';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // Installs
 // npm install directory-tree
 // npm i node-fetch
 
-const filepath = "/Volumes/colorado/splash/react"
+//const filepath = "/Volumes/colorado/splash/react"
+const filepath = process.env.WEB_LOCAL_FILE_PATH
 /*
 Data Generated out of meet programm - pdf's
 paste to subfolders
@@ -12,12 +16,12 @@ paste to subfolders
 - certificates
 */
 
-const wsClubServer="ubuntu.fritz.box"
+const wsClubServer = process.env.WEB_RESULT_BACKEND_SERVER
 /*
 this is backend of admin Server
 --> upload lxf file to the Result tab
 */
 
-const exportFileName="/Users/MFU/projects/private/schwimmen/resultdata/resources/downloads.json"
+const exportFileName = process.env.DEST_EXPORT_FILE_NAME
 
-get_file_data(filepath,wsClubServer,exportFileName)
+get_file_data(filepath, wsClubServer, exportFileName)
