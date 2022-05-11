72c18b801b90a6949e74b227bf2eef0c1aafc648
matthias <matthias.fuchs>
2022-05-11 21:53:49 +0200

    reduce results

--- data/reduceResults.js
+++ data/reduceResults.js
@@ -24,13 +24,21 @@ function getEmptyResult(place) {
 function clearPlaceAbove(resultJson, place) {
 
     var newResult = []
+    var numberresults = 0
 
     resultJson.map(result => {
+        if (numberresults < result.data.length) {
+            numberresults = result.data.length
+            console.log('<reduceResults> number results' + result.data.length)
+        }
+
         if (parseInt(result.place) < parseInt(place)) {
-            console.log(getEmptyResult(result.place))
+            console.log(result.data.length)
             newResult.push(getEmptyResult(result.place))
         } else {
-           newResult.push(result)
+            if (result.data.length > +numberresults - 1) {
+                newResult.push(result)
+            }
         }
     })
     return newResult
