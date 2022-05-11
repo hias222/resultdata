9969807e38ba71544f97497b16aa65bed94ecbe8
matthias <matthias.fuchs>
2022-05-11 22:08:40 +0200

    reduce

--- data/reduceResults.js
+++ data/reduceResults.js
@@ -26,18 +26,27 @@ function clearPlaceAbove(resultJson, place) {
     var newResult = []
     var numberresults = 0
 
+    var actualplace = 0
+
     resultJson.map(result => {
+
         if (numberresults < result.data.length) {
             numberresults = result.data.length
-            console.log('<reduceResults> number results' + result.data.length)
+            console.log('<reduceResults> number results ' + result.data.length)
         }
 
         if (parseInt(result.place) < parseInt(place)) {
-            console.log(result.data.length)
             newResult.push(getEmptyResult(result.place))
         } else {
             if (result.data.length > +numberresults - 1) {
-                newResult.push(result)
+                actualplace++
+                if (actualplace < parseInt(result.place)) {
+                    result.place = '' + actualplace
+                    console.log('<reduceResult.js> missing')
+                    newResult.push(result)
+                } else {
+                    newResult.push(result)
+                }
             }
         }
     })
