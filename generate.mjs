68df8c53a0b0f09010c556c7cee9e209d0e48aa2
matthias <matthiasfuchs@me.com>
2024-01-01 16:08:45 +0100

    update

--- generate.mjs
+++ generate.mjs
@@ -1,6 +1,12 @@
 import get_file_data from './scripts/getfilestructure.mjs';
 import dotenv from 'dotenv'
 
+if (process.argv.length === 2) {
+    console.error('Expected at least one argument! Sot MEeting name');
+    process.exit(1);
+  }
+
+const shortMeetingName= process.argv[2]
 dotenv.config()
 
 // Installs
@@ -8,7 +14,7 @@ dotenv.config()
 // npm i node-fetch
 
 //const filepath = "/Volumes/colorado/splash/react"
-const filepath = process.env.WEB_LOCAL_FILE_PATH
+const filepath = process.env.WEB_LOCAL_FILE_PATH + "/" + shortMeetingName
 /*
 Data Generated out of meet programm - pdf's
 paste to subfolders
@@ -22,6 +28,11 @@ this is backend of admin Server
 --> upload lxf file to the Result tab
 */
 
-const exportFileName = process.env.DEST_EXPORT_FILE_NAME
+const exportFileName = process.env.DEST_EXPORT_FILE_NAME + '/' + shortMeetingName + '/downloads.json' 
+
+console.info('using ' + filepath)
+console.info('Server ' + wsClubServer)
+console.info('exportFilename ' + exportFileName)
+console.info('')
 
 get_file_data(filepath, wsClubServer, exportFileName)
